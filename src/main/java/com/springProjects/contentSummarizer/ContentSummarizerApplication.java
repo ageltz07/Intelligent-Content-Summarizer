5e6ff93cc5ac96454cbfa650f3add3fcc56d876a
Adam Geltz <ageltz07@gmail.com>
2024-11-02 14:35:36 -0400

    Initial Commit

---
+++ src/main/java/com/springProjects/contentSummarizer/ContentSummarizerApplication.java
@@ -0,0 +1,13 @@
+package com.springProjects.contentSummarizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentSummarizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentSummarizerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springProjects/contentSummarizer/ContentSummarizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springProjects.contentSummarizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentSummarizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
