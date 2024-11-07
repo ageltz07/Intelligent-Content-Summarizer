e293a66ee6686ae7e4ab55346254aeb489ef7395
Adam Geltz <ageltz07@gmail.com>
2024-11-07 07:38:21 -0500

    Storing some small changes

--- src/main/java/com/springProjects/contentSummarizer/controller/SummaryController.java
+++ src/main/java/com/springProjects/contentSummarizer/controller/SummaryController.java
@@ -4,8 +4,6 @@ import com.springProjects.contentSummarizer.model.ContentRequest;
 import com.springProjects.contentSummarizer.model.Summary;
 import com.springProjects.contentSummarizer.repository.SummaryRepository;
 import com.springProjects.contentSummarizer.service.SummaryService;
-import com.springProjects.contentSummarizer.utility.OpenAIApiClient;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
@@ -22,8 +20,8 @@ public class SummaryController {
     }
 
     @PostMapping(value = "/summarize", consumes = "application/json", produces = "application/json")
-    public Summary generateSummary(@RequestBody ContentRequest request) {
-        String prompt = "Summarize the following text:\n\n" + request.getContent();
+    public String generateSummary(@RequestBody ContentRequest request) {
+        String prompt = "Summarize the following text with a Shakespearean writing style:\n\n" + request.getContent();
         String summaryText = summaryService.getSummary(prompt);
 
         Summary summary = new Summary();
@@ -31,6 +29,6 @@ public class SummaryController {
         summary.setSummaryText(summaryText);
         summary.setCreatedAt(LocalDateTime.now());
 
-        return summaryRepository.save(summary);
+        return summaryRepository.save(summary).getSummaryText();
     }
 }
--- src/main/java/com/springProjects/contentSummarizer/utility/OpenAIApiClient.java
+++ src/main/java/com/springProjects/contentSummarizer/utility/OpenAIApiClient.java
@@ -1,9 +1,7 @@
 package com.springProjects.contentSummarizer.utility;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
